eb5cdcea1c65eca4687df6423c32050782ec1b58
Garrett James <garrettjames55@gmail.com>
2019-11-02 18:05:57 -0400

    finished assignment

--- scripts/store.js
+++ scripts/store.js
@@ -37,6 +37,10 @@ function findAndDelete(id) {
   this.items.splice(itemIndex, 1);
 }
 
+function toggleCheckedFilter() {
+  this.hideCheckedItems = !this.hideCheckedItems;
+}
+
 export default {
   items,
   hideCheckedItems,
@@ -44,5 +48,6 @@ export default {
   addItem,
   findAndToggleChecked,
   findAndUpdateName,
-  findAndDelete
+  findAndDelete,
+  toggleCheckedFilter
 };
