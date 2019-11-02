effe7cf076d77031f051c0e7e03d1f342acaccf9
Garrett James <garrettjames55@gmail.com>
2019-11-02 14:44:13 -0400

    added CRUD functions to store.js

--- scripts/store.js
+++ scripts/store.js
@@ -3,7 +3,46 @@ import item from './item.js';
 const items = [];
 const hideCheckedItems = false;
 
+function findById(id) {
+  return this.items.find(item => item.id === id);
+}
+
+function addItem(name) {
+  try {
+    item.validateName(name);
+    this.items.push(item.create(name));
+  } catch (error) {
+    console.error(`Name not valid`);
+  }
+}
+
+function findAndToggleChecked(id) {
+  let item = this.findById(id);
+  item.checked = !item.checked;
+}
+
+function findAndUpdateName(id, newName) {
+  try {
+    item.validateName(newName);
+    let foundItem = this.findById(id);
+    foundItem.name = name;
+  } catch (error) {
+    console.error(`Cannot update name: ${error.message}`);
+  }
+}
+
+function findAndDelete(id) {
+  let deleteItem = this.findById(id);
+  let itemIndex = this.items.findIndex(item => item === deleteItem);
+  this.items.splice(itemIndex, 1);
+}
+
 export default {
   items,
-  hideCheckedItems
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete
 };
